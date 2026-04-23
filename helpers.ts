ac63579de11698e8f2643ed7327beaf454b6b653
Saiful33322 <139411301+Saiful33322@users.noreply.github.com>
2026-04-23 15:01:48 +0600

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T09:01:45.992Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-04-23T09:01:47.537Z
+Commit: Fix race condition in async operations
