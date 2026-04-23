3f53e5b2e2327facbdd735bc7579e2da6a9b095a
Saiful33322 <139411301+Saiful33322@users.noreply.github.com>
2026-04-23 15:01:17 +0600

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-23T09:00:56.663Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-23T09:01:16.672Z
+Commit: Patch API endpoint error handling
